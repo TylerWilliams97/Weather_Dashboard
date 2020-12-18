7c6e92edc421cb47b18d5c061c87daff5cdcff79
TylerWilliams97 <w.tyler1997@gmail.com>
2020-12-18 14:47:17 -0800

    added temp print

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $('#inputBtn').on('click',function() {
     
 var Cityname = $('#cityInput').val();
 var apiKey = "c65b51c4f8a8bdbc1e50518620b108c5";
-var weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+Cityname+"&appid="+apiKey;
+var weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+Cityname+"&units=imperial&cnt=5&appid="+apiKey;
    console.log(Cityname);
    
    $.ajax({
@@ -12,7 +12,21 @@ var weatherUrl = "https://api.openweathermap.org/data/2.5/forecast?q="+Cityname+
         method:"GET"
     }).then(function(Response){
         console.log(Response)
-    });
+
+
+        for (let i = 0; i < 5; i++) {
+           var c = Response.list[i].main.temp;
+         
+            console.log(c);
+              var createBlock = $('<div>').text(c+" F ");
+              $('#weatherblocks').append(createBlock);
+            
+        }
+   
+   
+    
+    
+     });
 
 });
 
